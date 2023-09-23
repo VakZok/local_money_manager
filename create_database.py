50abe3709a85c1525942a63f0cd6d1cccb2175d5
VakZok <tobias.krebs1999@gmx.de>
2023-09-23 23:52:09 +0200

    Added first draft of table to be created in my local money manager database

---
+++ create_database.py
@@ -0,0 +1,82 @@
+import mysql.connector
+
+# MySQL connection configuration
+config = {
+    'user': 'your_username',
+    'password': 'your_password',
+    'host': 'your_host',
+    'database': 'your_database'
+}
+
+# Establish a connection to the MySQL server
+connection = mysql.connector.connect(**config)
+
+# Create a cursor object to execute SQL queries
+cursor = connection.cursor()
+
+# SQL query to create a table
+create_table_query = '''
+CREATE TABLE transactions (
+    date DATE,
+    description VARCHAR(50),
+    value DOUBLE NOT NULL,
+    main_category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50) NOT NULL,
+    PRIMARY KEY (date, description),
+    CHECK (main_category IN (
+        'Transport',
+        'Haus/Wohnung',
+        'Versicherungen',
+        'Lebensmittel',
+        'Gesundheit',
+        'Sport',
+        'Kleidung',
+        'Persönlich',
+        'Soziales',
+        'Reisen',
+        'Bildung',
+        'Abbonements',
+        'Versicherungen',
+        'Spenden',
+        'Geschenke',
+        'Gehalt',
+        'Zinsen/Dividende',
+        'Verkauf'
+    )),
+    CHECK (subcategory IN (
+        'Allgemein',
+        'Auto',
+        'Öffis',
+        'Miete',
+        'Hypothek',
+        'Strom',
+        'Wasser',
+        'Gas',
+        'Möbel',
+        'Krankenversicherung',
+        'Privathaftpflichtversicherung',
+        'Berufsunfähigkeitsversicherung',
+        'Wohngebäudeversicherung',
+        'Kfz_Haftpflichtversicherung',
+        'Zahnzusatzversicherung',
+        'Rechtsschutzversicherung',
+        'Urlaub',
+        'Medikamente',
+        'Vorsorge',
+        'Behandlung',
+        'Sehhilfe',
+        'Mitgliedschaft'
+    ))
+)
+'''
+
+try:
+    # Execute the SQL query to create the table
+    cursor.execute(create_table_query)
+    print("Table 'example_table' created successfully.")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    # Close the cursor and the connection
+    cursor.close()
+    connection.close()
