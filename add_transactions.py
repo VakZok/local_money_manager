594f51db0331ba6dfd7d441ba9bc85dd1691961b
VakZok <tobias.krebs1999@gmx.de>
2023-09-24 11:00:01 +0200

    Moved transactions to a private file

--- add_transactions.py
+++ add_transactions.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import credentials
+import transactions
 
 # MySQL connection configuration
 config = {
@@ -15,13 +16,6 @@ connection = mysql.connector.connect(**config)
 # Create a cursor object to execute SQL queries
 cursor = connection.cursor()
 
-# New transaction entries
-transactions = [
-    {'date': '2023-09-23', 'description': 'Groceries', 'value': 50.00, 'main_category': 'Expenses', 'subcategory': 'Food'},
-    {'date': '2023-09-24', 'description': 'Gasoline', 'value': 40.00, 'main_category': 'Expenses', 'subcategory': 'Transport'},
-    {'date': '2023-09-25', 'description': 'Earnings', 'value': 1000.00, 'main_category': 'Income', 'subcategory': 'Salary'}
-]
-
 # SQL query to insert a new transaction
 insert_transactions_query = '''
 INSERT INTO transactions (date, description, value, main_category, subcategory)
@@ -30,7 +24,7 @@ VALUES (%(date)s, %(description)s, %(value)s, %(main_category)s, %(subcategory)s
 
 try:
     # Execute the SQL query to insert transactions
-    cursor.execute(insert_transactions_query)
+    cursor.execute(insert_transactions_query, transactions.transactions)
     print("Transactions inserted successfully.")
 except mysql.connector.Error as err:
     print(f"Error: {err}")
