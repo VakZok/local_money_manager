c493689f1753f64000e0b643ae47030ccfc0edf3
VakZok <tobias.krebs1999@gmx.de>
2023-09-24 11:54:30 +0200

    Added method for date conversion

---
+++ methods.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def convert_date(original_date):
+    # Define the input and output date formats
+    input_format = "%d.%m.%Y"
+    output_format = "%Y-%m-%d"
+
+    # Parse the original date using the input format
+    parsed_date = datetime.strptime(original_date, input_format)
+    # Format the parsed date to the output format
+    mysql_date = parsed_date.strftime(output_format)
