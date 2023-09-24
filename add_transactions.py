b0fc434316880e6449d170ec2ce485a06ec78160
VakZok <tobias.krebs1999@gmx.de>
2023-09-24 11:37:44 +0200

    Added connection.commit(), a loop for all single transactions and an update of the transaction value in case of a duplicate key

--- add_transactions.py
+++ add_transactions.py
@@ -20,15 +20,18 @@ cursor = connection.cursor()
 insert_transactions_query = '''
 INSERT INTO transactions (date, description, value, main_category, subcategory)
 VALUES (%(date)s, %(description)s, %(value)s, %(main_category)s, %(subcategory)s)
+ON DUPLICATE KEY UPDATE value = %(value)s;
 '''
 
 try:
     # Execute the SQL query to insert transactions
-    cursor.execute(insert_transactions_query, transactions.transactions)
+    for transaction in transactions.transactions:
+        cursor.execute(insert_transactions_query, transaction)
     print("Transactions inserted successfully.")
 except mysql.connector.Error as err:
     print(f"Error: {err}")
 finally:
     # Close the cursor and the connection
     cursor.close()
+    connection.commit()
     connection.close()
